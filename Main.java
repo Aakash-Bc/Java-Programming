3224dabdaf52c530e7c3f360be286ec898cee3ff
Aakash Budhathoki <134195553+Aakash-Bc@users.noreply.github.com>
2025-01-12 13:46:20 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter a letter: ");
+        char letter = scanner.next().toLowerCase().charAt(0);
+
+
+        if (!Character.isLetter(letter)) {
+            System.out.println("Invalid input. Please enter a letter.");
+        } else {
+
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+                System.out.println(letter + " is a vowel.");
+            } else {
+                System.out.println(letter + " is a consonant.");
+            }
+        }
+
+        scanner.close();
+    }
+}
