11df5bebb7ea9405e0853634d4bf4ffcf21bfc5c
Aakash Budhathoki <134195553+Aakash-Bc@users.noreply.github.com>
2025-01-12 13:49:35 +0545

    Update and rename Main.java to ToCalculateInternalMask.java

--- ToCalculateInternalMask.java
+++ ToCalculateInternalMask.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class ToCalculateInternalMask {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
