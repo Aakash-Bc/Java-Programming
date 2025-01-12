b05aeee3124630f2638f9b3ed97ff19e2321ab53
Aakash Budhathoki <134195553+Aakash-Bc@users.noreply.github.com>
2025-01-12 13:34:23 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Example of 'break':");
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6) {
+                System.out.println("Breaking the loop at i = " + i);
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("\nExample of 'continue':");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("\nExample of 'return':");
+        printMessageAndReturn();
+        System.out.println("This line will not execute because 'return' ends the program flow in the method.");
+    }
+
+    public static void printMessageAndReturn() {
+        System.out.println("This is a method with a return statement.");
+        return;
+    }
+}
